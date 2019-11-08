ab69a5d1c847be88166d4b2e30d495b3d8257614
Craig Fay <craigfay1@gmail.com>
2019-11-08 08:35:47 -0600

    Remove dist

--- dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var waverunner_1 = require("waverunner");
-waverunner_1.run(waverunner_1.testSuite(require('./schema.test')));
-//# sourceMappingURL=test.js.map
